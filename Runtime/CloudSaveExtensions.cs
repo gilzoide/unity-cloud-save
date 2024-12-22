72b0bd8a23a1723736a1fa32de313a6229493eeb
gilzoide <gilzoide@gmail.com>
2024-12-22 11:53:08 -0300

    Add ICloudSaveProvider.LoadDataAsync extension method

--- Runtime/CloudSaveExtensions.cs
+++ Runtime/CloudSaveExtensions.cs
@@ -8,22 +8,26 @@ namespace Gilzoide.CloudSave
     {
         public static Task<ISavedGame> SaveGameAsync(this ICloudSaveProvider cloudSaveProvider, string name, string text, Encoding encoding = null, SaveGameMetadata metadata = null, CancellationToken cancellationToken = default)
         {
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8;
-            }
-            byte[] bytes = encoding.GetBytes(text);
+            byte[] bytes = (encoding ?? Encoding.UTF8).GetBytes(text);
             return cloudSaveProvider.SaveGameAsync(name, bytes, metadata, cancellationToken);
         }
 
         public static async Task<string> LoadTextAsync(this ISavedGame savedGame, Encoding encoding = null, CancellationToken cancellationToken = default)
         {
-            if (encoding == null)
-            {
-                encoding = Encoding.UTF8;
-            }
             byte[] bytes = await savedGame.LoadDataAsync(cancellationToken);
-            return encoding.GetString(bytes);
+            return (encoding ?? Encoding.UTF8).GetString(bytes);
+        }
+
+        public static async Task<byte[]> LoadDataAsync(this ICloudSaveProvider cloudSaveProvider, string name, CancellationToken cancellationToken = default)
+        {
+            ISavedGame savedGame = await cloudSaveProvider.LoadGameAsync(name, cancellationToken);
+            return await savedGame?.LoadDataAsync(cancellationToken);
+        }
+
+        public static async Task<string> LoadTextAsync(this ICloudSaveProvider cloudSaveProvider, string name, Encoding encoding = null, CancellationToken cancellationToken = default)
+        {
+            ISavedGame savedGame = await cloudSaveProvider.LoadGameAsync(name, cancellationToken);
+            return await savedGame?.LoadTextAsync(encoding, cancellationToken);
         }
     }
 }
